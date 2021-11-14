d24bc2037b20080e08beeca9db35d129d494fd03
Markzhou2207 <32120051+Markzhou2207@users.noreply.github.com>
2021-11-14 14:25:17 +1100

    updated allowed host

--- projFive/learning_users/learning_users/settings.py
+++ projFive/learning_users/learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=3v!p_h!kkadd$-4i$i57!^rvn)fg@er&lyl2a&_ic666q6ix_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.']
 
 
 # Application definition
